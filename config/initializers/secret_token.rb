bf49ba3e67f02890064333c8b932a9cde6c40274
Peter Ching <peter.ching@gmail.com>
2014-03-10 17:25:43 -0400

    initial commit; fresh rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MDJDgallery::Application.config.secret_key_base = 'de102ea80b6d517837b17d75ada8446686e90ce7bb8a2716884c7aef988c82050c7af17b6ad3c95e3304db05114766c46a3f8cefcc43a4657c095e0bce553c0b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MDJDgallery::Application.config.session_store :cookie_store, key: '_MDJDgallery_session'
